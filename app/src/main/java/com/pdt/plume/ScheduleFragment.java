ccb7faedaa4d7165841eaff59c513e32f6099436
sumnermic <sumnermic@github.com>
2016-08-01 09:40:32 +0100

    0.06c Highlight Schedule

--- app/src/main/java/com/pdt/plume/ScheduleFragment.java
+++ app/src/main/java/com/pdt/plume/ScheduleFragment.java
@@ -10,13 +10,18 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v7.view.ActionMode;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.pdt.plume.data.DbContract;
 import com.pdt.plume.data.DbHelper;
@@ -27,8 +32,7 @@ import java.util.ArrayList;
 public class ScheduleFragment extends Fragment {
     String LOG_TAG = ScheduleFragment.class.getSimpleName();
     Utlility utlility = new Utlility();
-
-    ArrayList<Integer> highlightedItems = new ArrayList<>();
+    ListView listView;
 
 
     public ScheduleFragment() {
@@ -44,12 +48,13 @@ public class ScheduleFragment extends Fragment {
         DbHelper dbHelper = new DbHelper(getContext());
 
         // Initialise Schedule List
-        final ListView listView = (ListView) rootView.findViewById(R.id.schedule_list);
+        listView = (ListView) rootView.findViewById(R.id.schedule_list);
         final ScheduleAdapter mScheduleAdapter = new ScheduleAdapter(getContext(), R.layout.list_item_schedule, dbHelper.getCurrentDayScheduleArray());
-        if (listView != null){
+        if (listView != null) {
+            listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
             listView.setAdapter(mScheduleAdapter);
             listView.setOnItemClickListener(clickListener());
-            listView.setOnItemLongClickListener(longClickListener());
+            listView.setMultiChoiceModeListener(new ModeCallback());
 
             if (getResources().getBoolean(R.bool.isTablet))
                 listView.performItemClick(listView.getChildAt(0), 0, listView.getFirstVisiblePosition());
@@ -66,100 +71,85 @@ public class ScheduleFragment extends Fragment {
         });
 
 
-
         // Inflate the layout for this fragment
         return rootView;
     }
 
-    private Schedule generateDummySchedule(int count){
-        Resources resources = getResources();
-        int[] scheduleIcons = {
-                R.drawable.placeholder_sixtyfour,
-                R.drawable.placeholder_sixtyfour,
-                R.drawable.placeholder_sixtyfour
-        };
-
-        return new Schedule(
-                scheduleIcons[count],
-                resources.getStringArray(R.array.schedule_lessons)[count],
-                resources.getStringArray(R.array.schedule_teachers)[count],
-                resources.getStringArray(R.array.schedule_rooms)[count],
-                resources.getStringArray(R.array.schedule_time_ins)[count],
-                resources.getStringArray(R.array.schedule_time_outs)[count]);
-    }
-
-    public Schedule[] generateDummyScheduleArray(){
-        return new Schedule[]{
-                generateDummySchedule(0),
-                generateDummySchedule(1),
-                generateDummySchedule(2)
-        };
-    }
-
-    private void highlight(View view, int position){
-        //If the selected item is already highlighted, deselect it
-        ImageView scheduleIcon = (ImageView) view.findViewById(R.id.schedule_icon);
-        if (highlightedItems.contains(position)){
-            DbHelper dbHelper = new DbHelper(getContext());
-            dbHelper.getCurrentDayScheduleData();
-            Cursor cursor = dbHelper.getCurrentDayScheduleData();
-            if (cursor.moveToPosition(position)){
-                int imageId = cursor.getInt(cursor.getColumnIndex(DbContract.ScheduleEntry.COLUMN_ICON));
-                scheduleIcon.setImageResource(imageId);
-            }
-            Log.v(LOG_TAG, "" + position);
-            highlightedItems.remove(position);
-            highlightedItems.add(position, -1);
-        }
-        else {
-            Log.v(LOG_TAG, "" + position);
-            highlightedItems.add(position, position);
-            scheduleIcon.setImageDrawable(getResources().getDrawable(R.drawable.icon_checked));
-        }
-    }
-
-    public AdapterView.OnItemLongClickListener longClickListener(){
-        return new AdapterView.OnItemLongClickListener() {
-            @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
-                highlight(view, position);
-                return true;
-            }
-        };
-    }
-
-    public AdapterView.OnItemClickListener clickListener(){
+    public AdapterView.OnItemClickListener clickListener() {
 
         return new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // If no other items are highlighted, open fragment as normal
-                int nullCounter = -1;
-                for (int i = 0; i < highlightedItems.size(); i++){
-                    int integer = highlightedItems.get(i);
-                    if (i != -1)
-                        nullCounter++;
+                //Replace fragment if device is a tablet
+                if (getResources().getBoolean(R.bool.isTablet)) {
+                    ScheduleDetailFragment fragment = new ScheduleDetailFragment();
+                    getActivity().getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.detail_container, fragment)
+                            .commit();
                 }
-                Log.v(LOG_TAG, "Null Counter: " + nullCounter);
-                if (nullCounter < 0){
-                    //Replace fragment if device is a tablet
-                    if (getResources().getBoolean(R.bool.isTablet)){
-                        ScheduleDetailFragment fragment = new ScheduleDetailFragment();
-                        getActivity().getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.detail_container, fragment)
-                                .commit();
-                    }
-                    //Start a new activity if device is a phone
-                    else{
-                        Intent intent = new Intent(getActivity(), ScheduleDetailActivity.class);
-                        startActivity(intent);
-                    }
-                }
+                //Start a new activity if device is a phone
                 else {
-                    highlight(view, position);
+                    Intent intent = new Intent(getActivity(), ScheduleDetailActivity.class);
+                    startActivity(intent);
                 }
             }
         };
     }
+
+
+    private class ModeCallback implements ListView.MultiChoiceModeListener {
+
+        @Override
+        public void onItemCheckedStateChanged(android.view.ActionMode mode, int position, long id, boolean checked) {
+            final int checkedCount = listView.getCheckedItemCount();
+            switch (checkedCount) {
+                case 0:
+                    mode.setSubtitle(null);
+                    break;
+                case 1:
+                    mode.setSubtitle("One item selected");
+                    break;
+                default:
+                    mode.setSubtitle("" + checkedCount + " items selected");
+                    break;
+            }
+        }
+
+        @Override
+        public boolean onCreateActionMode(android.view.ActionMode mode, Menu menu) {
+            MenuInflater inflater = getActivity().getMenuInflater();
+            inflater.inflate(R.menu.menu_new_detail, menu);
+            mode.setTitle("Select Items");
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(android.view.ActionMode mode, Menu menu) {
+            return true;
+        }
+
+        @Override
+        public boolean onActionItemClicked(android.view.ActionMode mode, MenuItem item) {
+            switch (item.getItemId()) {
+                default:
+                    Toast.makeText(getActivity(), "Clicked " + item.getTitle(),
+                            Toast.LENGTH_SHORT).show();
+                    break;
+            }
+            return true;
+        }
+
+        @Override
+        public void onDestroyActionMode(android.view.ActionMode mode) {
+
+        }
+    }
+
+
+
+
+
+
+
 
 }
